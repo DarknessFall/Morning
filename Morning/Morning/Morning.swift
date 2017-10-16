c2f2886660646f3e2b6786593512bf4f52acb84b
Bruce Jackson <zysosx@gmail.com>
2017-10-16 19:05:24 +0800

    add base view

--- Morning/Morning/Morning.swift
+++ Morning/Morning/Morning.swift
@@ -8,11 +8,65 @@
 
 import UIKit
 
-protocol MorningAlert {
-    func show()
-    func dismiss()
+typealias MorningHandler = (Bool) -> ()
+
+class MorningAlert: UIView {
+
+    var contentView: UIView = UIView()
+
+    var dismissOnTouchOutside = true
+
+    ///Init Methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    ///Public Methods
+    func show(inView view: UIView = UIApplication.shared.keyWindow!) {
+        view.addSubview(self)
+
+        frame = view.bounds
+
+        view.addObserver(self, forKeyPath: "frame", context: nil)
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapped(tapGestureRecognizer:)))
+        addGestureRecognizer(tapGesture)
+    }
+
+    func dismiss() {
+
+    }
+
 }
 
-class Morning: UIView {
+///MARK: KVO
+extension MorningAlert {
+
+    override func observeValue(forKeyPath keyPath: String?,
+                               of object: Any?,
+                               change: [NSKeyValueChangeKey : Any]?,
+                               context: UnsafeMutableRawPointer?) {
+        if keyPath == "frame" {
+            frame = superview!.bounds
+        }
+    }
+
+}
+
+private extension MorningAlert {
+
+    @objc func tapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        guard dismissOnTouchOutside else {
+            return
+        }
+
+        dismiss()
+    }
 
 }
